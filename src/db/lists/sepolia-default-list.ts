759986a70acd5973c9a1b25253550c1a7f94b18c
Viktor Peredera <vitia2282@gmail.com>
2024-10-07 17:45:13 +0300

    feat: update sepolia default list

--- src/db/lists/sepolia-default-list.ts
+++ src/db/lists/sepolia-default-list.ts
@@ -4,7 +4,7 @@ export const sepoliaDefaultList = {
   version: {
     major: 1,
     minor: 0,
-    patch: 6,
+    patch: 7,
   },
   logoURI: "/token-list-placeholder.svg",
   name: "DEX223 Sepolia Default",
@@ -12,7 +12,7 @@ export const sepoliaDefaultList = {
     new Token(
       11155111,
       "0x51a3F4b5fFA9125Da78b55ed201eFD92401604fa",
-      "0xD3783257FD5E5b0CFA244eC14dc10B2263dC5f63",
+      "0x4939A61f6276Ff98f350fEd487FC8ffFE5Fce403",
       18,
       "TOT1",
       "Total Test 1",
@@ -21,7 +21,7 @@ export const sepoliaDefaultList = {
     new Token(
       11155111,
       "0x30C7A2261Ad72ad12393B075163Bf9a72a2dA4f8",
-      "0x1A57D43b2F1Aed52106614eE4D3359247bD1982a",
+      "0xA2370D7046993056ba53Cf89beca123330e8B494",
       6,
       "TOT2",
       "Total Test 2",
@@ -30,7 +30,7 @@ export const sepoliaDefaultList = {
     new Token(
       11155111,
       "0x18B32000CEaAe9BE1cec1cb6845EF7bba1c9DC02",
-      "0x77eBa0266AcCf29e80A753645FC964975ad25C75",
+      "0x882A5B2806D05001AD8eFC69CF68bAdB18640e6B",
       10,
       "TOT3",
       "Total Test 3",
@@ -39,7 +39,7 @@ export const sepoliaDefaultList = {
     new Token(
       11155111,
       "0xa083e42B1525c67A90Cb1628acBC99895dC0447B",
-      "0x2992B2Efcef1f63297a4C34404AF9b8B39E3f2Ef",
+      "0xbeAF0d7e6edF62d84B2d01c949F6f5797Be4258D",
       0,
       "TOT4Z",
       "Total Test 4Z",
