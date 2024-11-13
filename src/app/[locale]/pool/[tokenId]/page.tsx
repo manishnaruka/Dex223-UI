453eb3338c7fb66b6077051e479b04b2f6e9af0d
Exzender <alekn@mail.ru>
2024-11-13 08:39:23 +0100

    - radio button fix

--- src/app/[locale]/pool/[tokenId]/page.tsx
+++ src/app/[locale]/pool/[tokenId]/page.tsx
@@ -438,6 +438,7 @@ export default function PoolPage({
               {token0?.isNative ? null : (
                 <div className="flex flex-col gap-2 md:gap-3 mt-3">
                   <RadioButton
+                    className="bg-quaternary-bg"
                     isActive={token0Standard === Standard.ERC20}
                     onClick={() => setToken0Standard(Standard.ERC20)}
                     disabled={
@@ -453,6 +454,7 @@ export default function PoolPage({
                     </div>
                   </RadioButton>
                   <RadioButton
+                    className="bg-quaternary-bg"
                     isActive={token0Standard === Standard.ERC223}
                     onClick={() => setToken0Standard(Standard.ERC223)}
                     disabled={
@@ -494,6 +496,7 @@ export default function PoolPage({
               {token1?.isNative ? null : (
                 <div className="flex flex-col gap-2 mt-3">
                   <RadioButton
+                    className="bg-quaternary-bg"
                     isActive={token1Standard === Standard.ERC20}
                     onClick={() => setToken1Standard(Standard.ERC20)}
                     disabled={
@@ -511,6 +514,7 @@ export default function PoolPage({
                     </div>
                   </RadioButton>
                   <RadioButton
+                    className="bg-quaternary-bg"
                     isActive={token1Standard === Standard.ERC223}
                     onClick={() => setToken1Standard(Standard.ERC223)}
                     disabled={
--- src/components/buttons/RadioButton.tsx
+++ src/components/buttons/RadioButton.tsx
@@ -17,7 +17,7 @@ export default function RadioButton({
   return (
     <button
       className={clsxMerge(
-        "duration-200 text-14 md:text-16 h-10 flex px-3 md:px-4 lg:px-5 items-center rounded-2 group hocus:shadow hocus:shadow-green/60 gap-2 bg-quaternary-bg hocus:text-primary-text disabled:pointer-events-none disabled:opacity-50",
+        "duration-200 text-14 md:text-16 h-10 flex px-3 md:px-4 lg:px-5 items-center rounded-2 group hocus:shadow hocus:shadow-green/60 gap-2 bg-tertiary-bg hocus:text-primary-text disabled:pointer-events-none disabled:opacity-50",
         isActive ? "text-primary-text" : "text-secondary-text opacity-50",
         className,
       )}
@@ -25,13 +25,13 @@ export default function RadioButton({
     >
       <span
         className={clsx(
-          "flex-shrink-0 duration-200 w-4 h-4 rounded-full border relative bg-tertiary-bg justify-center items-center group-hocus:border-green",
+          "flex-shrink-0 duration-200 w-4 h-4 rounded-full border relative bg-tertiary-bg group-hocus:border-green",
           isActive ? "border-green" : "border-secondary-border",
         )}
       >
         <span
           className={clsx(
-            "flex-shrink-0 duration-200 w-2.5 h-2.5 rounded-full bg-green absolute inset-0 m-auto",
+            "duration-200 w-2.5 h-2.5 rounded-full bg-green absolute inset-0 m-auto",
             isActive ? "opacity-100 shadow shadow-green/60" : "opacity-0",
           )}
         />
