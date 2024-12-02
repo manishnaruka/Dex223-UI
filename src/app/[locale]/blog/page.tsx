21577e2cb0c2d9148f5f4917d77cc4943102a203
Viktor Peredera <vitia2282@gmail.com>
2024-12-02 17:21:24 +0200

    feat: update anchor to Link

--- src/app/[locale]/blog/page.tsx
+++ src/app/[locale]/blog/page.tsx
@@ -1,4 +1,5 @@
 import { posts } from "@/app/[locale]/blog/test-posts";
+import { Link } from "@/navigation";
 
 export default function BlogPage() {
   return (
@@ -7,9 +8,9 @@ export default function BlogPage() {
         return (
           <div key={post.id} className="bg-tertiary-bg rounded-2 p-4 flex gap-3">
             {post.title}
-            <a href={`/blog/${post.id}`} className="text-green underline hocus:text-green-hover">
+            <Link href={`/blog/${post.id}`} className="text-green underline hocus:text-green-hover">
               Link to article
-            </a>
+            </Link>
           </div>
         );
       })}
