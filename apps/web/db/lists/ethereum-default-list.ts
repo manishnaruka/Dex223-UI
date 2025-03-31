5d8ccef04a87d0f9dce3b3bc32a453240bb628d3
Dexaran <dexaran820@gmail.com>
2025-03-31 14:13:45 +0100

    Create ethereum-default-list.ts

---
+++ apps/web/db/lists/ethereum-default-list.ts
@@ -0,0 +1,101 @@
+export const mainnetDefaultList = {
+  version: {
+    major: 1,
+    minor: 0,
+    patch: 1,
+  },
+  logoURI: "/token-list-placeholder.svg",
+  name: "DEX223 ETH Default",
+  tokens: [
+    {
+      address0: "0xdAC17F958D2ee523a2206206994597C13D831ec7", // ERC-20
+      address1: "0xB8f0a8FCCB9F1d3d287A35643E93b8A7ee5E6980", // ERC-223
+      name: "Tether USD",
+      logoURI: "/images/tokens/USDT.svg",
+      chainId: 1,
+      decimals: 6,
+      symbol: "USDT",
+    },
+    {
+      address0: "0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48", // ERC-20
+      address1: "0xdc87CFa91A4D1A2CF0F74B8ebCE2c7FB1C00BD5e", // ERC-223
+      name: "USD Coin",
+      logoURI: "/images/tokens/USDC.svg",
+      chainId: 1,
+      decimals: 6,
+      symbol: "USDC",
+    },
+    {
+      address0: "0x514910771af9ca656af840dff83e8264ecf986ca", // ERC-20
+      address1: "0x8c8E029ef1CB647094163E353Cf61246AD9037f2", // ERC-223
+      name: "Chainlink",
+      logoURI: "",
+      chainId: 1,
+      decimals: 18,
+      symbol: "LINK",
+    },
+    {
+      address0: "0x2af5d2ad76741191d15dfe7bf6ac92d4bd912ca3", // ERC-20
+      address1: "0xEf5c5dEc0aA64165499c3090a395d984f9d4394f", // ERC-223
+      name: "UNUS SED LEO",
+      logoURI: "",
+      chainId: 1,
+      decimals: 18,
+      symbol: "LEO",
+    },
+    {
+      address0: "0x95ad61b0a150d79219dcf64e1e6cc01f0b64c4ce", // ERC-20
+      address1: "0x22c0c40a3f6c16F0D443A1BFDE1efb81a04a0480", // ERC-223
+      name: "Shiba Inu",
+      logoURI: "",
+      chainId: 1,
+      decimals: 18,
+      symbol: "SHIB",
+    },
+    {
+      address0: "0x3593d125a4f7849a1b059e64f4517a86dd60c95d", // ERC-20
+      address1: "0x1B706f516D889ECF12dDeaA2eFb06F88BDc305a2", // ERC-223
+      name: "Mantra",
+      logoURI: "",
+      chainId: 1,
+      decimals: 18,
+      symbol: "Om",
+    },
+    {
+      address0: "0x54D2252757e1672EEaD234D27B1270728fF90581", // ERC-20
+      address1: "0x6068CEfC0b5111E9D22274b9E17aebd3810Fe31A", // ERC-223
+      name: "Bitget token",
+      logoURI: "",
+      chainId: 1,
+      decimals: 18,
+      symbol: "BGB",
+    },
+    {
+      address0: "0x6982508145454ce325ddbe47a25d4ec3d2311933", // ERC-20
+      address1: "0x2b6df9ab1dFf8750f7C9fC52C4c7574a35b52036", // ERC-223
+      name: "Pepe",
+      logoURI: "",
+      chainId: 1,
+      decimals: 18,
+      symbol: "PEPE",
+    },
+    {
+      address0: "0x75231f58b43240c9718dd58b4967c5114342a86c", // ERC-20
+      address1: "0xDB7C7A7324e8d1AB0245156aD14c6f32CbD64532", // ERC-223
+      name: "OKB",
+      logoURI: "",
+      chainId: 1,
+      decimals: 18,
+      symbol: "OKB",
+    },
+    {
+      address0: "0x3c3a81e81dc49a522a592e7622a7e711c06bf354", // ERC-20
+      address1: "0x18783eF1c23ab1a9acA3227Ba61732292aFfA0cf", // ERC-223
+      name: "Mantle",
+      logoURI: "",
+      chainId: 1,
+      decimals: 18,
+      symbol: "MNT",
+    },
+  ],
+};
